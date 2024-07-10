f59b3ab8f776ef7aaca2a98f02b05dac525eeb89
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-11 02:05:28 +0300

    change columns to Mapped

--- user_service/app/models/user.py
+++ user_service/app/models/user.py
@@ -2,6 +2,7 @@ import uuid
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 
@@ -9,9 +10,9 @@ from .base import Base
 class User(Base):
     __tablename__ = "users"
 
-    user_id = Column(
-        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
+    user_id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    age = Column(Integer)
+    username: Mapped[str] = mapped_column(String(32), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(32), nullable=False, unique=True)
+    age: Mapped[int] = mapped_column(Integer, nullable=False)

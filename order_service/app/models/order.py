1a12b842a7f8d6ae38be98f6f194ee49906c59b0
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-11 02:09:35 +0300

    change columns to Mapped

--- order_service/app/models/order.py
+++ order_service/app/models/order.py
@@ -1,7 +1,8 @@
 import uuid
 
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import ForeignKey, Integer, String
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 
@@ -9,6 +10,10 @@ from .base import Base
 class Order(Base):
     __tablename__ = "orders"
 
-    order_id = Column(Integer, primary_key=True, default=uuid.uuid4, index=True)
-    description = Column(String, index=True)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.user_id"))
+    order_id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, default=uuid.uuid4, index=True
+    )
+    description: Mapped[str] = mapped_column(String, index=True)
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("users.user_id")
+    )

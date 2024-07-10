a5609ffd0c8b64d73b4de255a39939e970855c8c
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-10 13:28:47 +0300

    create schema for user

---
+++ user_service/app/schemas.py
@@ -0,0 +1,31 @@
+import uuid
+
+from pydantic import BaseModel, ConfigDict
+
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+    age: int
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserUpdate(UserCreate):
+    pass
+
+
+class UserUpdatePartial(UserCreate):
+    username: str | None = None
+    email: str | None = None
+    age: int | None = None
+
+
+class User(UserCreate):
+    model_config = ConfigDict(from_attributes=True)
+    user_d: uuid.UUID
+    name: str
+    email: str
+    age: int

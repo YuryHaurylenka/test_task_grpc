b25a004dc16072eebc440dd699d53d3aa44939fa
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-11 03:57:06 +0300

    create dependency which return order by id

---
+++ order_service/app/dependencies.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, Path, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from crud import order as crud_order
+from .core import db_helper
+from .models import Order
+
+
+async def order_by_id(
+    order_id: Annotated[int, Path],
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+) -> Order:
+    order = await crud_order.get_order(session=session, order_id=order_id)
+    if order is not None:
+        return order
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Order {order_id} not found!",
+    )

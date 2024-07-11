12df992113383975f888813f3774a1c3c71855b7
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-11 16:50:41 +0300

    add unique and autoincrement to order_id, remove default value

--- order_service/app/models/order.py
+++ order_service/app/models/order.py
@@ -11,8 +11,11 @@ class Order(Base):
     __tablename__ = "orders"
 
     order_id: Mapped[int] = mapped_column(
-        Integer, primary_key=True, default=uuid.uuid4, index=True
+        Integer,
+        primary_key=True,
+        index=True,
+        autoincrement=True,
     )
     user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), nullable=False)
-    order_name: Mapped[str] = mapped_column(String, nullable=False)
+    order_name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
     description: Mapped[str] = mapped_column(String, index=True, nullable=False)

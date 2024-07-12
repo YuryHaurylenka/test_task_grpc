23d936699801bfe8c6ddb588eb5ef39345ebe804
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-12 19:00:10 +0300

    create new UserServicer class

--- user_service/grpc/user_service.py
+++ user_service/grpc/user_service.py
@@ -1,48 +1,34 @@
-import asyncio
 import logging
+from concurrent import futures
 
 import grpc
-from sqlalchemy.ext.asyncio import AsyncSession
 
-import user_pb2 as pb2
-import user_pb2_grpc as pb2_grpc
-from user_service.app.crud.user import get_user
+from user_service.app.core import db_helper
+from user_service.app.models import User
+from user_service.grpc import user_pb2 as pb2, user_pb2_grpc as pb2_grpc
 
 
-class UserServicer(pb2_grpc.UserServiceServicer):
-    async def GetUserResponse(
-        self,
-        request: pb2.UserRequest,
-        context: grpc.aio.ServicerContext,
-    ) -> pb2.GetUserResponse:
-        user_id = request.user_id
-        async with AsyncSession() as session:
-            user = await get_user(session, user_id)
-            if user:
-                return pb2.GetUserResponse(
-                    user_id=user.user_id,
-                    username=user.username,
-                    email=user.email,
-                    age=user.age,
-                )
-            else:
+class UserServiceServicer(pb2_grpc.UserServiceServicer):
+    async def GetUser(self, request, context):
+        session = db_helper.get_scoped_session()
+        async with session() as s:
+            user = await s.get(User, request.user_id)
+            if user is None:
+                context.set_details(f"User with id {request.user_id} not found")
                 context.set_code(grpc.StatusCode.NOT_FOUND)
-                context.set_details(f"User with id {user_id} not found")
-                return pb2.GetUserResponse()
+                return pb2.UserResponse()
+            return pb2.UserResponse(
+                user_id=str(user.user_id),
+                username=user.username,
+                email=user.email,
+                age=user.age,
+            )
 
 
-async def serve() -> None:
-    server = grpc.aio.server()
-    pb2_grpc.add_UserServiceServicer_to_server(UserServicer(), server)
-    listen_addr = "[::]:50051"
-    server.add_insecure_port(listen_addr)
-    logging.info("Starting server on %s", listen_addr)
+async def serve_grpc():
+    server = grpc.aio.server(futures.ThreadPoolExecutor(max_workers=10))
+    pb2_grpc.add_UserServiceServicer_to_server(UserServiceServicer(), server)
+    server.add_insecure_port("0.0.0.0:50051")
+    logging.info("Starting gRPC server on port 50051...")
     await server.start()
-    print("Server started. Listening on port 50051.")
     await server.wait_for_termination()
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    logging.info("User Server Starter...")
-    asyncio.run(serve())

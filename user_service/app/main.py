41892d52ac7f25ffc0dce0560c74172b98abe3d2
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-12 19:33:39 +0300

    add start_uvicorn , start_servers funcs

--- user_service/app/main.py
+++ user_service/app/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, status
+import asyncio
+import logging
 from starlette.responses import RedirectResponse
 
+from fastapi import FastAPI, status
+import uvicorn
 from user_service.app.core.config import settings
 from user_service.app.views import router as api_v1_router
+from user_service.grpc.user_service import serve_grpc
 
 app = FastAPI()
 app.include_router(
@@ -18,3 +22,20 @@ app.include_router(
 )
 async def root():
     return RedirectResponse(url="/docs")
+
+
+async def start_uvicorn():
+    config = uvicorn.Config("main:app", host="0.0.0.0", port=8000, reload=True)
+    server = uvicorn.Server(config)
+    await server.serve()
+
+
+async def start_servers():
+    grpc_task = asyncio.create_task(serve_grpc())
+    uvicorn_task = asyncio.create_task(start_uvicorn())
+    await asyncio.gather(grpc_task, uvicorn_task)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(start_servers())

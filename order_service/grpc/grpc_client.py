3a5bbc1102435b515a4b8522b8d5d198a35a7462
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-12 19:33:50 +0300

    add error handlers to check_user_exists

--- order_service/grpc/grpc_client.py
+++ order_service/grpc/grpc_client.py
@@ -12,5 +12,9 @@ async def check_user_exists(user_id: uuid.UUID) -> bool:
     async with grpc.aio.insecure_channel("localhost:50051") as channel:
         stub = user_pb2_grpc.UserServiceStub(channel)
         request = user_pb2.UserRequest(user_id=str(user_id))
-        response = await stub.GetUser(request)
-        return response.user_id == str(user_id)
+        try:
+            response = await stub.GetUser(request)
+            return response.user_id == str(user_id)
+        except grpc.aio.AioRpcError as e:
+            print(f"Error while checking user existence: {e}")
+            return False

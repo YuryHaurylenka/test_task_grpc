ca7dda768e41669cda44f2726d52b6d63f202873
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-12 19:34:08 +0300

    add running uvicorn with running main.py

--- order_service/app/main.py
+++ order_service/app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
@@ -14,3 +15,7 @@ app.include_router(
 @app.get("/", include_in_schema=False)
 async def root():
     return RedirectResponse(url="/docs")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
